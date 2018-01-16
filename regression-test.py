026e90937c68cf222586430caf41371c97352474
Adarsh Dave <adarsh.r.dave@PCLOADLETTER.wv.cc.cmu.edu>
2018-01-16 10:16:04 -0500

    Updated environment and added a scratch py script

---
+++ regression-test.py
@@ -0,0 +1,14 @@
+from sklearn import linear_model
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+'''
+reg = linear_model.LinearRegression()
+reg.fit([[0,0],[1,1],[2,2]],[0,1,2])
+#print reg.coef_
+x_predict = np.array([[np.linspace(0,2,100),np.linspace(0,2,100)]])
+plt.scatter([0,1,2],[0,1,2],color='black')
+plt.show()
+'''
+
